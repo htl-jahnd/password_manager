d80e622fb78a4c9aef1397ce97bba4230f73996d
David <david.jahn2000@gmail.com>
2018-11-13 23:27:05 +0100

    design and classpath changes

--- PasswordMamnager/src/pkgMain/Password_Manager_MainAppl.java
+++ PasswordMamnager/src/pkgMain/Password_Manager_MainAppl.java
@@ -9,25 +9,25 @@ import javafx.stage.Stage;
 public class Password_Manager_MainAppl extends Application
 {
 
-    public static void main(String[] args) throws Exception
-    {
-    	launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage)
-    {
-	try
+	public static void main(String[] args) throws Exception
 	{
-	    Parent root = FXMLLoader.load(getClass().getResource("ressources/GUI_Login.fxml"));
-	    primaryStage.setTitle("Copy Files");
-	    primaryStage.setScene(new Scene(root));
-	    
-	    primaryStage.show();
-	} catch (Exception e)
-	{
-	    pkgMisc.ExceptionHandler.hanldeUnexpectedException(e);
+		launch(args);
 	}
-    }
+
+	@Override
+	public void start(Stage primaryStage)
+	{
+		try
+		{
+			Parent root = FXMLLoader.load(getClass().getResource("ressources/SignUp.fxml"));
+			primaryStage.setTitle("Sign Up");
+			primaryStage.setScene(new Scene(root));
+
+			primaryStage.show();
+		} catch (Exception e)
+		{
+			pkgMisc.ExceptionHandler.hanldeUnexpectedException(e);
+		}
+	}
 
 }
